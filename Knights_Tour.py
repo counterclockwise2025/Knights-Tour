5ea950dc7a24f0b54da15576bd340fb8c3aa6b66
svd246 <svd246@gmail.com>
2020-02-16 15:57:49 -0500

    code work

--- Knights_Tour.py
+++ Knights_Tour.py
@@ -7,8 +7,17 @@ Knights Tour
 #define the main program and create a 2D array for the chest board
 def main():
     chess_board = [['[ * ]' for y in range (8)] for x in range (8)]
+    save_file = open("Tour.txt", 'w')
     xMove = input("What is your x move for the Knight: ")
-    yMove = input("What is your y Move for the Knight: ")
+    yMove = input("What is your y move for the Knight: ")
+
+    not_done = True
+    while not_done:
+        print_board()
+
+    save_file.close()
+
+
 
 #print board
 def print_board(chess_board):
@@ -16,6 +25,7 @@ def print_board(chess_board):
         each_line = ''
         for x in range (8):
             each_line += '{} '.format(chess_board[x][y])
+
         print(each_line)
 
 #create a knight class
@@ -38,10 +48,29 @@ class Knight:
 def not_finished():
     #in dev
 
-class Node():
+class Node:
     #in dev, define head node
-    def __init__(self):
+    def __init__(self, data):
+        self.data = data
+        self.next_node = None
 
-class LinkedList:
+        return
+
+class SingleLinkedList:
     #in dev
-    def __init__(self):
+    def __init__(self, item):
+        self.head = None
+        self.tail = None
+        self.item = item
+        '''
+        if not isinstance(item, Node):
+            item = Node(item)
+        '''
+        if self.head is None:
+            self.head = item
+        else:
+            self.tail.next = item
+
+        self.tail = item
+
+        return
