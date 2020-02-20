b92d7c5e721457695f2f3ac2c3771c7ec5463918
svd246 <svd246@gmail.com>
2020-02-20 14:38:53 -0500

    backtrack progressing

--- Knights_Tour.py
+++ Knights_Tour.py
@@ -8,13 +8,17 @@ Knights Tour
 def main():
     chess_board = [['[ * ]' for y in range (8)] for x in range (8)]
     save_file = open("Tour.txt", 'w')
-    xMove = input("What is your x move for the Knight: ")
-    yMove = input("What is your y move for the Knight: ")
+    print("Welcome to the Knights Tour, this program will show you the shortest path the Knight piece will take from your desired start position. \n")
+    x_move = input("What is your X coordinate start position for the Knight: ")
+    y_move = input("What is your Y coordinate start position for the Knight: ")
     #do not know if this line is even correct on storing the value in the LinkedList
-    SingleLinkedList(xMove, yMove)
+    #SingleLinkedList(xMove, yMove)
 
+    #While the program is not done it will still run through the program until there is a solution found
     not_done = True
     while not_done:
+        Knight.valid_move()
+        Knight.backtrack()
         print_board()
 
     save_file.close()
@@ -39,29 +43,41 @@ def not_finished():
 
 #create a knight class with functions to help move the knight with implemented back tracking
 class Knight:
-    def __init__(self, x, y, chess_board):
+    def __init__(self, x, y, chess_board, move_count):
         self.x = x
         self.y = y
         self.chess_board = chess_board
+        self.move_count = move_count
         self.stack = []
 
         return
 
-    def move(self, x, y, chess_board):
+    def valid_move(self, x, y, chess_board):
         #checks the board bounds and size to make sure knight will not be out of bound
-        if(x >= 0 and y >= 0 and x < 8 and y < 8 and chess_board[x][y] == -1):
+        if(x >= 0 and y >= 0 and x < 8 and y < 8 and chess_board[x][y] == 0):
             return True
         return False
-        
-        if():
 
-        return
+    def backtrack(self, x, y, chess_board, move_count):
+        #8 possible ways for the knight to move
+        pos_x_move = [2, 1, -1, -2, -2, -1, 1, 2]
+        pos_y_move = [1, 2, 2, 1, -1, -2, -2, -1]
 
-    def backtrack(self):
-        #in dev
-        if():
+        if(move_count == 32):
+            return True
 
-        return
+        for p in range (0, 8):
+            next_x = x + pos_x_move[p]
+            next_y = y + pos_y_move[p]
+
+            if(valid_move(next_x, next_y, chess_board)):
+                chess_board [next_x][next_y] = move_count
+                if(backtrack(chess_board, next_x, next_y, move_count + 1, pos_x_move, pos_y_move)):
+                    stack.append(move_count)
+                    return True
+                else #finish this statement and figure it out
+                    stack.pop(move_count)
+
 
 class Node:
     #in dev
@@ -73,9 +89,9 @@ class Node:
 
 class SingleLinkedList:
     #in dev
-    def __init__(self, item):
+    def __init__(self, NULL, item):
         self.head = None
-        self.tail = None
+        self.tail = NULL
         self.item = item
         '''
         if not isinstance(item, Node):
