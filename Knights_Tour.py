ccedd6999a05fad34597bc9ebfb5af75e4920f4b
svd246 <svd246@gmail.com>
2020-02-20 22:03:42 -0500

    Program working

--- Knights_Tour.py
+++ Knights_Tour.py
@@ -4,104 +4,159 @@ Samip Vaidh
 Knights Tour
 '''
 
-#define the main program and create a 2D array for the chest board
-def main():
-    chess_board = [['[ * ]' for y in range (8)] for x in range (8)]
-    save_file = open("Tour.txt", 'w')
-    print("Welcome to the Knights Tour, this program will show you the shortest path the Knight piece will take from your desired start position. \n")
-    x_move = input("What is your X coordinate start position for the Knight: ")
-    y_move = input("What is your Y coordinate start position for the Knight: ")
-    #do not know if this line is even correct on storing the value in the LinkedList
-    #SingleLinkedList(xMove, yMove)
-
-    #While the program is not done it will still run through the program until there is a solution found
-    not_done = True
-    while not_done:
-        Knight.valid_move()
-        Knight.backtrack()
-        print_board()
-
-    save_file.close()
-
-    return
+import random
 
 #print board
 def print_board(chess_board):
-    for y in range (8):
+    for col in range (8):
         each_line = ''
-        for x in range (8):
-            each_line += '{} '.format(chess_board[x][y])
+        for row in range (8):
+            each_line += '[ {} ] '.format(chess_board[col][row])
 
         print(each_line)
 
-    return
-
-def not_finished():
-    #in dev
-
-    return
-
-#create a knight class with functions to help move the knight with implemented back tracking
+#knight class with functions to help move the knight with implemented back tracking
 class Knight:
-    def __init__(self, x, y, chess_board, move_count):
-        self.x = x
-        self.y = y
+    def __init__(self, col, row, chess_board):
+        self.x = col
+        self.y = row
         self.chess_board = chess_board
-        self.move_count = move_count
-        self.stack = []
 
+        self.chess_board[self.x][self.y] = 'k'
+        self.stack = []
         return
 
-    def valid_move(self, x, y, chess_board):
-        #checks the board bounds and size to make sure knight will not be out of bound
-        if(x >= 0 and y >= 0 and x < 8 and y < 8 and chess_board[x][y] == 0):
-            return True
-        return False
+    #function condition that will tell the while loop if the condition is met
+    def is_done(self):
+        for row in range(8):
+            for col in range(8):
+                if self.chess_board[col][row] == '*': #if '*' is found then the loop will continue
+                    return False
+        return True
 
-    def backtrack(self, x, y, chess_board, move_count):
-        #8 possible ways for the knight to move
-        pos_x_move = [2, 1, -1, -2, -2, -1, 1, 2]
-        pos_y_move = [1, 2, 2, 1, -1, -2, -2, -1]
+    #finds all the possible moves the knight can prouduce
+    def find_moves(self, position = None):
+        if position is None:
+            col = self.x
+            row = self.y
+        else:
+            col, row = position
 
-        if(move_count == 32):
-            return True
+        #stores all possible moves into a array
+        moves = []
+        #possible moves for the knight, hard coded into tuples
+        possible_moves = [(2,1), (1,2), (-1, 2), (-2,1), (-2, -1), (-1, -2), (1, -2), (2, -1)]
+        
+        #loops through the list of possible moves array 
+        for element in possible_moves: 
+            x_move, y_move = element 
+            #making sure that the knight piece does not go out of bounds
+            if col + x_move < 8 and col + x_move > -1 and row + y_move < 8 and row + y_move > -1 and self.chess_board[col + x_move][row + y_move] == '*':
+                moves.append((col + x_move, row + y_move)) #add this to the moves array since it can be done after this check
+        return moves #returns the moves array at the end with all possible moves
+    
+    #uses the moves array to actually move the knight piece by random
+    def make_move(self, moves):
+        #looks at the array of possible moves and picks a random tuple as it's move
+        move = random.choice(moves)
+        col, row = (self.x, self.y)
+        self.stack.append((col, row)) #append to the stack as visited
+        self.chess_board [col][row] = 'v' #set the status of the sqaure to visited
 
-        for p in range (0, 8):
-            next_x = x + pos_x_move[p]
-            next_y = y + pos_y_move[p]
+        #make the knights position to set as 'k'
+        col, row = move
+        self.x = col
+        self.y = row
+        self.chess_board[col][row] = 'k'
 
-            if(valid_move(next_x, next_y, chess_board)):
-                chess_board [next_x][next_y] = move_count
-                if(backtrack(chess_board, next_x, next_y, move_count + 1, pos_x_move, pos_y_move)):
-                    stack.append(move_count)
-                    return True
-                else #finish this statement and figure it out
-                    stack.pop(move_count)
+    def backtrack(self):
+        #setting tuple to be x, y
+        position = (self.x, self.y)
+        removed =[position] #store removed position including the current spot
+        while len(self.find_moves(position)) == 0:
+            position = self.stack.pop()
+            removed.append(position)
 
+        #position knight to the current sqaure it is on
+        self.make_move(self.find_moves(position))
+
+        #looks back into the removed array and checks the previous possible moves back to * since it cannot go back to those till it finds a valid move
+        for element in removed:
+            col, row = element
+            self.chess_board[col][row] = '*' #set whatever was backtracked to unvisited
 
 class Node:
-    #in dev
-    def __init__(self, data):
+    def __init__(self, data = None):
         self.data = data
         self.next_node = None
 
-        return
-
 class SingleLinkedList:
-    #in dev
-    def __init__(self, NULL, item):
-        self.head = None
-        self.tail = NULL
-        self.item = item
-        '''
-        if not isinstance(item, Node):
-            item = Node(item)
-        '''
-        if self.head is None:
-            self.head = item
+    def __init__(self, head = None):
+        self.head = head
+        self.item = Node
+    
+    def get_head(self):
+        return self.head
+
+    def delete_head(self):
+        if self.head is not None:
+            self.head = self.head.next_node
+
+    def add(self, item):
+        if self.head is not None:
+            current = self.head
+            while current.next_node is not None:
+                current = current.next_node
+            current.next_node = Node(item)
         else:
-            self.tail.next = item
+            self.head = Node(item)
 
-        self.tail = item
+#define the main program and create a 2D array for the chest board
+if __name__ == '__main__':
+    save_file = open("Tour.txt", 'w')
+    lst = SingleLinkedList()
+    res = ''
 
-        return
+    while res != 'EXIT':
+
+        intro = """
+        Welcome to the Knights Tour, this program will show you the shortest path the Knight piece will take from your desired start position. Things to note are...
+
+        * - unvisited square
+        v - knight has visited
+        k - current position of the knight
+        """
+        print(intro)
+        x_move = int(input("What is your X coordinate start position for the Knight: "))
+        y_move = int(input("What is your Y coordinate start position for the Knight: "))
+
+        while x_move < 1 and x_move > 8 and y_move < 1 and y_move > 8:
+            x_move = int(input("What is your X coordinate start position for the Knight: "))
+            y_move = int(input("What is your Y coordinate start position for the Knight: "))
+
+        lst.add((x_move - 1, y_move - 1))
+        res = input("Type EXIT to leave the program and watch the tour, or press ENTER to start a new tour. ")
+
+    while lst.head is not None:
+        col, row = lst.get_head().data #tuple, unpacking values from initial position tuple 
+        lst.delete_head()
+        chess_board = [['*' for row in range (8)] for col in range (8)]
+        k = Knight(col, row, chess_board) 
+        '''
+        for in range 32:
+            moves.k.find_moves
+            for move in moves
+        '''
+        #will print each move till all of the board is set to 'v' for visited
+        while not k.is_done():
+            moves = k.find_moves() #list of moves
+            if len(moves) > 0:
+                k.make_move(moves) #picking moves and adding to the stack
+            else:
+                k.backtrack() #going back to find a possible move that is valid
+            print_board(chess_board) #print the board each time a move is made
+            #add spacing between borad prints
+            print()
+            print()
+
+        save_file.close()
